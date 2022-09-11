eaa6f35767d3c6986a4a3c6c795ca00c10a5981b
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-11 10:32:44 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,9 +16,11 @@ for (c = 48; c <= 57; c++)
 {
 putchar(c);
 if (c != 57)
-{        
-putchar(',');																                 
-putchar(' ');															
+		{
+			putchar(',');
+			putchar(' ');
+		}
+  char(' ');															
 }
 }
 putchar('\n');
