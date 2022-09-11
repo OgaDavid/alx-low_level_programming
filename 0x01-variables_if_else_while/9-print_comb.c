247687e705791220ee8e57f2f720d138b0633b07
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-09-11 10:29:32 -0700

    Coding

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,7 +16,7 @@ for (c = 48; c <= 57; c++)
 {
 putchar(c);
 if (c != 57)
-{																	putchar(',');																putchar(' ');
+{																	putchar(',');																                 putchar(' ');
 																
 }
 }
