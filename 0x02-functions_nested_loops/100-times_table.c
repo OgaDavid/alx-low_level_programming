cc14ac96f9d71ab45fd27aeeac4e994739dfaa1d
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-13 15:39:11 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  * @n: number of the times table
  */
 
-void print_times_table(int n)	
+void print_times_table(int n)
 {
 int i, j, k;
 if (n >= 0 && n <= 15)
@@ -18,7 +18,7 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} 
+}
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -26,7 +26,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} 
+}
 else if (k >= 10 && k < 100)
 {
 _putchar(',');
@@ -34,7 +34,7 @@ _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} 
+}
 else if (k >= 100)
 {
 _putchar(',');
