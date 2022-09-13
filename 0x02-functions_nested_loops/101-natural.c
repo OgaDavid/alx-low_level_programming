4dd0a50d7f4dfab184a0edfe55a769c6db9fdacd
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-13 15:44:08 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,7 +6,7 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
- 
+
 int main(void)
 {
 unsigned long int sum3, sum5, sum;
@@ -21,7 +21,8 @@ for (i = 0; i < 1024; ++i)
 if ((i % 3) == 0)
 {
 sum3 = sum3 + i;
-} else if ((i % 5) == 0)
+} 
+else if ((i % 5) == 0)
 {
 sum5 = sum5 + i;
 }
