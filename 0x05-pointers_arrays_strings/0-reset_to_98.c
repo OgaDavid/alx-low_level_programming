18fceb470cd31238715f78fa22efbb8cda4954dd
OgaDavid <oluwademiladedavidoga@gmail.com>
2022-09-19 03:01:32 -0700

    File 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,67 @@
+main.h.txt
+
+Who has access
+
+A
+
+System properties
+
+Type
+
+Text
+
+Size
+
+412 bytes
+
+Storage used
+
+412 bytes
+
+Location
+
+pointers arrays and strings
+
+Owner
+
+Antony Bahati
+
+Modified
+
+23:46 by Antony Bahati
+
+Opened
+
+02:58 by me
+
+Created
+
+23:50
+
+No description
+
+Viewers can download
+
+#include "main.h"
+
+
+
+/**
+
+*reset_to_98 - resets the value of given integer to 98
+
+*@n: parameter
+
+*Return : nothing
+
+*/
+
+
+
+void reset_to_98(int *n)
+
+{
+
+	*n = 98;
+
+}
