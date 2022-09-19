d2d2dd430f3c5004f7f4b8870775483646b1270d
demi.coder <104001201+OgaDavid@users.noreply.github.com>
2022-09-19 03:05:34 -0700

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,81 +1,21 @@
-main.h.txt
-
-Who has access
-
-A
-
-System properties
-
-Type
-
-Text
-
-Size
-
-412 bytes
-
-Storage used
-
-412 bytes
-
-Location
-
-pointers arrays and strings
-
-Owner
-
-Antony Bahati
-
-Modified
-
-23:46 by Antony Bahati
-
-Created
-
-23:50
-
-No description
-
-Viewers can download
-
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
 /**
-
 * void prototypes()
-
 * int prototypes()
-
 */
 
-
-
 void reset_to_98(int *n);
-
 void swap_int(int *a, int *b);
-
 int _strlen(char *s);
-
 void _puts(char *str);
-
 void print_rev(char *s);
-
 void rev_string(char *s);
-
 void puts2(char *str);
-
 void puts_half(char *str);
-
 void print_array(int *a, int n);
-
 char *_strcpy(char *dest, char *src);
-
 int _atoi(char *str);
-
-
 
 #endif /*MAIN_H*/
